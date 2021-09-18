52978ed7d5405a4b302984959a2ec0acac285858
Abhay Saini <90948162+abhaycodee@users.noreply.github.com>
2021-09-18 11:32:08 +0530

    Add files via upload

---
+++ area of cicclr.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	
+int r;
+float circumference, area, pi=3.14;
+//we know that arae of cicle is area= pi*r*r
+
+// circumferece of cicle is given by the formula 2*pi*r
+printf("enter the radius of circle\n");
+scanf("%d",&r);
+pi=3.14;
+area=pi*r*r;
+printf("area of circle is = %f\n",area);
+
+// circumference of given radius circle is
+
+
+circumference=2*pi*r;
+printf("circumference of circle= %f",circumference);
+	
+	
+	
+	
+	
+	
+}
